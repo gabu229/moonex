fec806b73ba407597812173e0c30efbbd414fff9
Gabriel Orie <oriegabriel16@gmail.com>
2024-11-25 23:07:36 +0100

    Init data & types

---
+++ src/lib/types.ts
@@ -0,0 +1,16 @@
+export interface ComparisonPoint {
+  comparison: string;
+  moonex: boolean;
+  uniswap: boolean;
+}
+  
+export interface Feature {
+  title: string;
+  description: string;
+  icon: string;
+}
+  
+export interface FAQ {
+  question: string;
+  answer: string;
+}

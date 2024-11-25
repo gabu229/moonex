6a56d81f92c6c2418251d3eba725644c6ccafdce
Gabriel Orie <oriegabriel16@gmail.com>
2024-11-25 23:05:56 +0100

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <LandingPage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

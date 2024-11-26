97f48560e59b00f6aae25abbfa9ea573e3e22627
Gabriel Orie <oriegabriel16@gmail.com>
2024-11-26 14:38:59 +0100

    Init SmoothScroll && Clean Assets

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import LandingPage from "./pages/LandingPage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+import "./App.css";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
+
 function App() {
+  const ref = useRef(null);
+
+  const options = {
+    smooth: true,
+  };
+
   return (
-    <>
+    <LocomotiveScrollProvider options={options} containerRef={ref} watch={[]}>
       <Navbar />
-      <LandingPage />
-      <Footer />
-    </>
+      <div ref={ref} data-scroll data-scroll-container>
+        <LandingPage />
+        <Footer />
+      </div>
+    </LocomotiveScrollProvider>
   );
 }
 
